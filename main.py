3cc6a1c61aae940de506b86ed51d001e74410bc8
password <moto198720@yandex.ru>
2022-09-03 00:52:48 +0300

    ДЗ

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import re
 import bs4
 import requests
 from fake_useragent import UserAgent
 ua = UserAgent()
 
-KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+KEYWORDS = ['дизайн', 'фото', 'web', 'python', 'Jenkins', 'Guido']
 
 base_url = 'https://habr.com'
 url = base_url + '/ru/all/'
@@ -17,11 +18,12 @@ soup = bs4.BeautifulSoup(text, features='html.parser')
 
 articles = soup.find_all("article")
 
+
 for article in articles:
     words = article.find_all(class_='tm-article-body tm-article-snippet__lead')
     words = [word.text.strip() for word in words]
     for word in words:
-        if any(KEYWORDS for word in words):
+        if any(element in word for element in KEYWORDS):
             title = (f"Название статьи - '{article.find('h2').find('span').text}'")
             data = article.find(class_='tm-article-snippet__datetime-published').find('time').attrs['datetime']
             data1 = f"Дата статьи - {data[0:10]} {data[12:19]}"
